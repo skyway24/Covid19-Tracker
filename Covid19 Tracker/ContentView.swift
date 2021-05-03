3d40d26cd69d05230411f47f9590086d71084dca
Akash_Verma <akashverma2404@gmail.com>
2021-05-03 19:43:40 +0530

    Initial commit

---
+++ Covid19 Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19 Tracker
+//
+//  Created by Akash Verma on 02/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid19 Tracker/CovidFetchRequest.swift
@@ -0,0 +1,16 @@
+//
+//  CovidFetchRequest.swift
+//  Covid19 Tracker
+//
+//  Created by Akash Verma on 02/05/21.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+
+
+class CovidFetchRequest: ObservableObject{
+    
+}
