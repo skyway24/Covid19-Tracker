b06f96817eb710316b489dafeb7928a6330da3a3
Akash_Verma <akashverma2404@gmail.com>
2021-05-06 20:33:58 +0530

    Create TotalDataView.swift

---
+++ Covid19 Tracker/Views/TotalDataView.swift
@@ -0,0 +1,44 @@
+//
+//  TotalDataView.swift
+//  Covid19 Tracker
+//
+//  Created by Akash Verma on 05/05/21.
+//
+
+import SwiftUI
+
+struct TotalDataView: View {
+    
+    var totalData: TotalData
+    
+    
+    var body: some View {
+       
+        VStack{
+            HStack{
+                
+                TotalDataCard(number: totalData.confirmed.formatNumber(), name: "Confirmed", color: .blue)
+                TotalDataCard(number: totalData.critical.formatNumber(), name: "Critical",color: .yellow)
+                TotalDataCard(number: totalData.deaths.formatNumber(), name: "Deaths",color: .red)
+                
+            }
+            HStack{
+                
+                TotalDataCard(number: String(format: "%.2f", totalData.fatalityRate), name: "Death %", color: .red)
+                TotalDataCard(number: totalData.recovered.formatNumber(), name: "Recovered",color: .green)
+                TotalDataCard(number: String(format: "%.2f", totalData.recoveredRate), name: "Recovery %",color: .green)
+                
+            }
+            
+        }.frame(height: 170).padding(10)
+        
+        
+        
+    }
+}
+
+struct TotalDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        TotalDataView(totalData: testTotalData)
+    }
+}
